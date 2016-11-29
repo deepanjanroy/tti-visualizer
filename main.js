20b697b88f39a9062ae4c970e624031eacc6b01e
Deepanjan Roy <dproy@google.com>
2016-11-28 23:16:04 -0500

    zoom levels

--- main.js
+++ main.js
@@ -8,19 +8,37 @@ function draw(){
   var drawShadowRect = false;
   var shadowRect = null;
   var persistentRects = [];
-  var zoomLevel = 100;
+  var zoomLevel = 1;
+  var leftX = 0;
+
+  // Turns x in screen coordinates - translates and zooms.
+  function tz(x) {
+    return (x - leftX)* zoomLevel;
+  }
+
+  function invtz(x) {
+    return (x / zoomLevel) + leftX;
+  }
+
+  // Zooms a length.
+  function zoom(l) {
+    return l * zoomLevel;
+  }
+
+  function invzoom(l) {
+    return l / zoomLevel;
+  }
 
   function drawPersistentRects(ctx) {
     ctx.fillStyle = "rgb(200,0,200)";
     for (var rect of persistentRects) {
-      ctx.fillRect(rect.start, 5, rect.end - rect.start, 40);
-      ctx.strokeRect(rect.start, 5, rect.end - rect.start, 40);
+      ctx.fillRect(tz(rect.start), 5, zoom(rect.end - rect.start), 40);
+      ctx.strokeRect(tz(rect.start), 5, zoom(rect.end - rect.start), 40);
     }
   }
 
   function drawLoop() {
     if (canvas.getContext) {
-      console.log(zoomLevel);
       var ctx = canvas.getContext("2d");
       ctx.clearRect(0, 0, 1000, 50);
 
@@ -28,35 +46,44 @@ function draw(){
 
       if (showRect) {
         ctx.fillStyle = "rgb(200,0,200)";
-        ctx.fillRect (50, 5, 50, 40);
+        ctx.fillRect (tz(50), 5, tz(50), 40);
       }
 
       if (drawShadowRect) {
         ctx.fillStyle = "rgba(0, 150, 0, .5)";
-        ctx.fillRect(shadowRect.start, 5, shadowRect.end - shadowRect.start, 40);
+        ctx.fillRect(tz(shadowRect.start), 5, zoom(shadowRect.end - shadowRect.start), 40);
       }
 
       ctx.beginPath();
-      for (var x = 0; x < 1000; x+= 50) {
+      for (var x = 0; x < 1000; x += 50) {
         ctx.moveTo(x, 0);
         ctx.lineTo(x, 20);
       }
       ctx.stroke();
 
-      for (var x = 0; x < 1000; x+=50) {
+      for (var x = 0; x < 1000; x += 50) {
         ctx.fillStyle = "rgb(0,0,0)";
-        ctx.fillText("" + x, x, 10, 40);
+        var xValue = invtz(x).toFixed(2);
+        ctx.fillText("" + xValue, x, 10, 40);
       }
 
     }
     window.requestAnimationFrame(drawLoop);
   }
 
+  function getMousePos(e) {
+    var rect = canvas.getBoundingClientRect();
+    return {x: e.clientX - rect.left, y: e.clientY - rect.top};
+  }
+
   canvas.addEventListener("mousedown", event => {
+    // Gets rid of the annoying cursor when dragging.
     event.preventDefault();
+
+    var pos = getMousePos(event);
     console.log(event);
     drawShadowRect = true;
-    shadowRect = {start: event.x, end: event.x};
+    shadowRect = {start: invtz(pos.x), end: invtz(pos.x)};
   });
 
   canvas.addEventListener("mouseup", event => {
@@ -81,9 +108,10 @@ function draw(){
   canvas.addEventListener("mousemove", event => {
     // maybe this can be optimized.
     mouseInCanvas = true;
+    var pos = getMousePos(event);
 
     if (drawShadowRect) {
-      shadowRect.end = event.x;
+      shadowRect.end = invtz(pos.x);
     }
   });
 
